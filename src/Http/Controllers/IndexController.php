d56b9a84ad7a9efcddc4b029029002829c2d8639
Roman Sidash <sidash.roman@mail.ru>
2022-11-13 21:29:48 +0600

    feat: add index controller

---
+++ src/Http/Controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Slim\Http\ServerRequest;
+use Slim\Http\Response;
+use Slim\Views\Twig;
+
+class IndexController
+{
+    public function home(ServerRequest $request, Response $response)
+    {
+        $username = isset($_GET['name']) ? $_GET['name'] : 'guest';
+
+        $view = Twig::fromRequest($request);
+
+        return $view->render($response, 'home.twig', ['name' => $username]);
+    }
+}
